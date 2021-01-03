3614e70a9cd7225109657f7328a033f0f5dde7ee
Rajesh <ykrajsh@outlook.com>
2021-01-03 16:47:39 +0530

    adding Disk and attached in vm

--- main.tf
+++ main.tf
@@ -66,7 +66,24 @@ resource "azurerm_windows_virtual_machine" "vm" {
  source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
+}
+
+resource "azurerm_managed_disk" "D_drive" {
+  name                 = "${var.Virtual_Machine_name[1]}-D-Drive"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "50"
+
+
+}
+resource "azurerm_virtual_machine_data_disk_attachment" "add-disk" {
+  managed_disk_id    = azurerm_managed_disk.D_drive.id
+  virtual_machine_id = azurerm_windows_virtual_machine.vm.id
+  lun                = "10"
+  caching            = "ReadWrite"
 }
